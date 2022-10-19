be380f0d88ead560628bd80b01a0e1a58dcbfba2
ahmetson <ahmetson@zoho.com>
2022-10-19 16:21:16 +0300

    Add a function that verifies missing environment variables

---
+++ src/env.ts
@@ -0,0 +1,13 @@
+export const verify_env = function () {
+    if (!process.env.SDS_ORGANIZATION_NAME) {
+        throw 'Missing SDS_ORGANIZATION_NAME environment variable';
+    }
+    if (!process.env.SDS_PROJECT_NAME) {
+        throw 'Missing SDS_PROJECT_NAME environment variable';
+    }
+
+    let host = process.env.SDS_GATEWAY_HOST!
+    if (!host) {
+        throw `Missing 'SDS_GATEWAY_HOST' environment variable`;
+    }
+}
