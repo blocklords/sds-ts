f0863535497b09e6e2c112741579532febf4bbcd
ahmetson <ahmetson@zoho.com>
2023-03-13 19:29:42 +0300

    include hardhat and trufflle

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import SmartcontractData from "./utils/smartcontract-data";
 import { TYPES as SmartcontractDataTypes } from "./utils/smartcontract-data";
 import { Smartcontract } from "./smartcontract";
+import { Hardhat } from "./hardhat";
+import { Truffle } from "./truffle";
 import { BundleOptions } from "./bundle-options";
 import * as Gateway from "./sdk/gateway";
 import { SmartcontractDeveloperRequest as Request } from "./sdk/message/smartcontract_developer_request";
@@ -14,6 +16,8 @@ export {
     Gateway,
     Request,
     Smartcontract,
+    Hardhat,
+    Truffle,
     Provider,
     BundleOptions
 }
