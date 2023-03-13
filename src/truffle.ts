be1b41ca8b1489517f9a3d46071f0ef1f9cd6a80
ahmetson <ahmetson@zoho.com>
2023-03-13 19:13:54 +0300

    truffle global variables are set during truffle initiation

--- src/truffle.ts
+++ src/truffle.ts
@@ -7,45 +7,53 @@ import { Smartcontract } from "./smartcontract";
  * https://trufflesuite.com/truffle/
  */
 export class Truffle extends Smartcontract {
-  /**
-   * Create a new smartcontract with the topic.
-   * The topic's organization and project parameters are fetched from environment variables.
-   * @param group Smartcontract Group
-   * @param name Smartcontract Name should match to the name as its defined in the code.
-   * @example Assume that "SomeErc20.sol" file has `contract SomeErc20 {}`
-   * Then the `name` parameter should be `SomeErc20`.
-   * @throws exception if the environment variables are missing.
-   * @requires SDS_ORGANIZATION_NAME environment variable. Example: seascape.
-   * @requires SDS_PROJECT_NAME environment variable. Example: `uniswap`.
-   * @requires SDS_GATEWAY_HOST environment variable. Example: tcp://localhost:4001
-   */
-  constructor(group: string, name: string) {
-    super(group, name);
-  }
+    private web3: any;
+    private deployer: any;
+    private contract: any;
+
+    /**
+     * Create a new smartcontract with the topic.
+     * The topic's organization and project parameters are fetched from environment variables.
+     * @param group Smartcontract Group
+     * @param name Smartcontract Name should match to the name as its defined in the code.
+     * @param deployer The passed Truffle.Deployer object in the migration file
+     * @param contract The smartcontract artifact
+     * @param web3 a Web3js library initiated and ready to use
+     * @example Assume that "SomeErc20.sol" file has `contract SomeErc20 {}`
+     * Then the `name` parameter should be `SomeErc20`.
+     * @throws exception if the environment variables are missing.
+     * @requires SDS_ORGANIZATION_NAME environment variable. Example: seascape.
+     * @requires SDS_PROJECT_NAME environment variable. Example: `uniswap`.
+     * @requires SDS_GATEWAY_HOST environment variable. Example: tcp://localhost:4001
+     */
+    constructor(group: string, name: string, deployer: any, contract: any, web3: any) {
+        super(group, name);
+
+        this.deployer = deployer;
+        this.contract = contract;
+        this.web3 = web3;
+    }
 
   /**
    * Deploys smartcontract on the blockchain, then registers it on SeascapeSDS.
-   * @param deployer The passed Truffle.Deployer object in the migration file
-   * @param contract The smartcontract artifact
-   * @param web3 a Web3js library initiated and ready to use
    * @param constructorArguments 
    */
-  async deploy(deployer: any, contract: any, web3: any, deployer_address: string, constructorArguments: Array<any>) {
-    let network_id = (await web3.eth.net.getId()).toString();
+  async deploy(constructorArguments: Array<any>) {
+    let network_id = (await this.web3.eth.net.getId()).toString();
     this.set_network_id(network_id);
 
     // deploying smartcontract.
-    await deployer.deploy(contract, ...constructorArguments);
+    await this.deployer.deploy(this.contract, ...constructorArguments);
 
-    let abi = contract.abi;
+    let abi = this.contract.abi;
     if (!abi) {
       throw `error: can not find a smartcontract ABI. Make sure that smartcontrat name in .sol file is ${this.topic.name}`;
     }
 
     console.log(`${this.topic.name} deployed successfully!`);
 
-    let address = contract.address;
-    let txid = contract.transactionHash;
+    let address = this.contract.address;
+    let txid = this.contract.transactionHash;
     console.log(`'${this.topic.name}' address ${address}`);
     console.log(`'${this.topic.name}' txid    ${txid}`);
 
@@ -55,7 +63,7 @@ export class Truffle extends Smartcontract {
       txid: txid,
       abi: abi,
     });
-    message = await message.sign(deployer_address, web3);
+    message = await message.sign("", this.web3);
 
     console.log(`Sending 'register_smartcontract' command to SDS Gateway`);
     console.log(`The message to send to the user: `, message.toJSON());
@@ -77,13 +85,13 @@ export class Truffle extends Smartcontract {
    * @param deployer_address of the address that deployed the smartcontract
    * @param contract Smartcontract artifact
    */
-  async register(address: string, txid: string, web3: any, deployer_address: string, contract: any) {
-    this.topic.network_id = (await web3.eth.net.getId()).toString();
+  async register(address: string, txid: string) {
+    this.topic.network_id = (await this.web3.eth.net.getId()).toString();
 
     console.log(`'${this.topic.name}' address ${address}`);
     console.log(`'${this.topic.name}' txid    ${txid}`);
 
-    let abi = contract.abi;
+    let abi = this.contract.abi;
     if (!abi) {
       throw `failed to get the smartcontract abi`;
     }
@@ -93,7 +101,7 @@ export class Truffle extends Smartcontract {
       txid: txid,
       abi: abi,
     });
-    message = await message.sign(deployer_address, web3);
+    message = await message.sign("", this.web3);
 
     console.log(`Sending 'register_smartcontract' command to SDS Gateway`);
     
